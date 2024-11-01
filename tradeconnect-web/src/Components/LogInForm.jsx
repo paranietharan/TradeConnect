01d245585f599746bb84cdb0465fe78d227e2a3f
paranietharan <paranietharan20@gmail.com>
2024-11-01 11:05:09 +0530

    Login form work done

--- tradeconnect-web/src/Components/LogInForm.jsx
+++ tradeconnect-web/src/Components/LogInForm.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import styles from "./styles/LogInForm.module.css";
+import { useState } from 'react';
 
 function LoginForm() {
+    const [passwordVisible, setPasswordVisible] = useState(false);
+
+    const togglePasswordVisibility = () => {
+        setPasswordVisible(!passwordVisible);
+    };
+
     return (
         <div className={styles.loginForm}>
             <h1 className={styles.loginFormTitle}>Welcome back!</h1>
@@ -10,7 +17,21 @@ function LoginForm() {
             </p>
             <form className={styles.loginFormForm}>
                 <input className={styles.loginFormInput} type="email" placeholder="Email" />
-                <input className={styles.loginFormInput} type="password" placeholder="Password" />
+                {/* <input className={styles.loginFormInput} type="password" placeholder="Password" /> */}
+                <div className={styles.passwordContainer}>
+                    <input
+                        className={styles.loginFormInput}
+                        type={passwordVisible ? "text" : "password"}
+                        placeholder="Password"
+                    />
+                    <button
+                        type="button"
+                        className={styles.togglePasswordButton}
+                        onClick={togglePasswordVisibility}
+                    >
+                        {passwordVisible ? "Hide" : "Show"}
+                    </button>
+                </div>
                 <button className={styles.loginFormButton} type="submit">Login</button>
             </form>
         </div>
