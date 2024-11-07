5fe05e7754da5babe9dff104313607c449f81d77
paranietharan <paranie20@gmail.com>
2024-11-07 19:16:33 +0530

    Sign in page design changed

--- tradeconnect-web/src/Pages/LoginPage.jsx
+++ tradeconnect-web/src/Pages/LoginPage.jsx
@@ -3,19 +3,25 @@ import styles from "./styles/LoginPage.module.css";
 import LoginLogo from "../assets/shopping_login.svg";
 import LoginForm from "../Components/LogInForm";
 import SignUpText from "../Components/SignUpText";
+import TopNavigationBar from "../Components/TopNavigationBar";
 
 function LoginPage() {
     return (
-        <div className={styles.loginContainer}>
-            <div className={styles.loginPageLeftContainer}>
-                <img src={LoginLogo} alt="Login Logo" />
+        <div className={styles.loginPage}>
+            <div className={styles.topNavigationBarContainer}>
+                <TopNavigationBar />
             </div>
+            <div className={styles.loginContainer}>
+                <div className={styles.loginPageLeftContainer}>
+                    <img src={LoginLogo} alt="Login Logo" />
+                </div>
 
-            <div className={styles.loginPageRightContainer}>
-                <div className={styles.loginForm}>
-                    <LoginForm />
+                <div className={styles.loginPageRightContainer}>
+                    <div className={styles.loginForm}>
+                        <LoginForm />
 
-                    <SignUpText />
+                        <SignUpText />
+                    </div>
                 </div>
             </div>
         </div>
