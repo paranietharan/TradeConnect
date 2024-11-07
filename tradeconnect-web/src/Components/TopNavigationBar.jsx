70cf690974d4ae12d603c2eea57d3599b570d5c6
paranietharan <paranie20@gmail.com>
2024-11-07 18:52:11 +0530

    bootstrap icons added

--- tradeconnect-web/src/Components/TopNavigationBar.jsx
+++ tradeconnect-web/src/Components/TopNavigationBar.jsx
@@ -1,9 +1,32 @@
 import styles from './styles/TopNavigationBar.module.css';
+import eCommerceLogo from '../assets/e-commerce-logo.png';
+import { Link } from 'react-router-dom';
 
 function TopNavigationBar() {
     return (
         <div className={styles.topNavigationBarContainer}>
-            
+            <div className={styles.logoContainer}>
+                <img src={eCommerceLogo} alt='logo' />
+            </div>
+
+            <div className={styles.navLinksContainer}>
+                <Link to='/'>Home</Link>
+                <Link to='/contact'>Products</Link>
+                <Link to='/about'>About</Link>
+                <Link to='/login'>Login</Link>
+            </div>
+
+            <div className={styles.searchBarContainer}>
+                <input type='text' placeholder='Search for products...' />
+                <button>
+                    <i class="bi bi-search"></i>
+                </button>
+            </div>
+
+            <div className={styles.cartFavContainer}>
+                <Link to='/cart'>Cart</Link>
+                <Link to='/favorites'>Favorites</Link>
+            </div>
         </div>
     )
 }
