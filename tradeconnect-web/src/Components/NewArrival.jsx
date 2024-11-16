4326a5a302326c5f6c352c4df900cd3d632e3bce
paranietharan <paranie20@gmail.com>
2024-11-16 21:18:44 +0530

    new arrival change

--- tradeconnect-web/src/Components/NewArrival.jsx
+++ tradeconnect-web/src/Components/NewArrival.jsx
@@ -1,37 +1,38 @@
 import React, { useRef } from 'react';
 import styles from './styles/Arrival.module.css';
+import { Link } from 'react-router-dom';
 
 const newArrivals = [
     {
         id: 1,
         image: 'https://via.placeholder.com/200x150',
         link: '/product/1',
-        title: 'New Arrival 1'
+        title: 'New Arrival 1',
     },
     {
         id: 2,
         image: 'https://via.placeholder.com/200x150',
         link: '/product/2',
-        title: 'New Arrival 2'
+        title: 'New Arrival 2',
     },
     {
         id: 3,
         image: 'https://via.placeholder.com/200x150',
         link: '/product/3',
-        title: 'New Arrival 3'
+        title: 'New Arrival 3',
     },
     {
         id: 4,
         image: 'https://via.placeholder.com/200x150',
         link: '/product/4',
-        title: 'New Arrival 4'
+        title: 'New Arrival 4',
     },
     {
         id: 5,
         image: 'https://via.placeholder.com/200x150',
-        link: '/product/4',
-        title: 'New Arrival 4'
-    }
+        link: '/product/5',
+        title: 'New Arrival 5',
+    },
 ];
 
 function NewArrival() {
@@ -48,18 +49,29 @@ function NewArrival() {
     return (
         <div className={styles.newArrivalContainer}>
             <h1 className={styles.newArrivalTitle}>New Arrivals!</h1>
-            <button className={`${styles.scrollButton} ${styles.left}`} onClick={scrollLeft}>
+            <button
+                className={`${styles.scrollButton} ${styles.left}`}
+                onClick={scrollLeft}
+            >
                 &lt;
             </button>
             <div className={styles.newArrivalGrid} ref={scrollContainerRef}>
-                {newArrivals.map(item => (
+                {newArrivals.map((item) => (
                     <div key={item.id} className={styles.newArrivalItem}>
-                        <img src={item.image} alt={item.title} className={styles.newArrivalImage} />
-                        <a href={item.link} className={styles.newArrivalLink}>{item.title}</a>
+                        <img
+                            src={item.image}
+                            alt={item.title}
+                            className={styles.newArrivalImage}
+                            style={{ cursor: 'pointer' }}
+                            onClick={() => window.location.href = item.link}
+                        />
                     </div>
                 ))}
             </div>
-            <button className={`${styles.scrollButton} ${styles.right}`} onClick={scrollRight}>
+            <button
+                className={`${styles.scrollButton} ${styles.right}`}
+                onClick={scrollRight}
+            >
                 &gt;
             </button>
         </div>
